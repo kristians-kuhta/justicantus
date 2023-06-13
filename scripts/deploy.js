c7f0d51dc41bef2b142da65aadf14dcdb326bbde
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-06-13 18:55:22 +0300

    Amend deploy script to write to the frontend contracts dir

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,27 @@
 const hre = require('hardhat');
+const fs = require('fs');
+const path = require('path');
+
+function saveFrontendFiles(platform) {
+  const contractsDir = path.join(__dirname, "/../frontend/src/contracts");
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/contract-addresses.json",
+    JSON.stringify({ Platform: platform.address }, null, 2)
+  );
+
+  // `artifacts` is a helper property provided by Hardhat to read artifacts
+  const PlatformArtifact = artifacts.readArtifactSync("Platform");
+  fs.writeFileSync(
+    contractsDir + "/Platform.json",
+    JSON.stringify(PlatformArtifact, null, 2)
+  );
+
+  console.log(`Artifacts written to ${contractsDir} directory`);
+}
 
 async function main() {
   const Platform = await hre.ethers.getContractFactory('Platform');
@@ -8,6 +31,8 @@ async function main() {
   const platform = await Platform.deploy(VRF_COORDINATOR, SUBSCRIPTION_ID, KEY_HASH);
 
   await platform.deployed();
+
+  saveFrontendFiles(platform);
 
   console.log(
     `Platform with coordinator "${VRF_COORDINATOR}", subscription ID "${SUBSCRIPTION_ID}", and key hash of "${KEY_HASH}" deployed to ${platform.address}`
