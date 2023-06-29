88dcca97e6280ffa4f8cfa94982fcf5045d3e5f8
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-06-29 23:44:56 +0300

    Remove all handlers except Artist creation handler

--- justicantus-subgraph/src/platform.ts
+++ justicantus-subgraph/src/platform.ts
@@ -1,59 +1,23 @@
 import {
-  OwnershipTransferred as OwnershipTransferredEvent,
-  RegistrationCreated as RegistrationCreatedEvent,
   ResourceRegistered as ResourceRegisteredEvent
-} from "../generated/Platform/Platform"
-import {
-  OwnershipTransferred,
-  RegistrationCreated,
-  ResourceRegistered
-} from "../generated/schema"
+} from "../generated/Platform/Platform";
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRegistrationCreated(
-  event: RegistrationCreatedEvent
-): void {
-  let entity = new RegistrationCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.resourceType = event.params.resourceType
-  entity.requestId = event.params.requestId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import { Artist } from "../generated/schema";
 
 export function handleResourceRegistered(event: ResourceRegisteredEvent): void {
-  let entity = new ResourceRegistered(
+  // NOTE: currently only Artist resources are supported.
+  //       If we decide to add other resources, this logic will have to branch based on resourceType.
+  if (event.params.resourceType !== 1) return;
+
+  const { account, id, data } = event.params;
+
+  let artist = new Artist(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.account = event.params.account
-  entity.resourceType = event.params.resourceType
-  entity.Platform_id = event.params.id
-  entity.data = event.params.data
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  artist.account = account;
+  artist.title = data;
+  artist.platformId = id;
 
-  entity.save()
+  artist.save();
 }
