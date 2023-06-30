e73f27d9e0ad83e842a30dd0e261d8487eae295e
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-06-30 15:33:41 +0300

    Add subgraph unit tests and their utilities

--- justicantus-subgraph/tests/platform-utils.ts
+++ justicantus-subgraph/tests/platform-utils.ts
@@ -1,87 +1,38 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
-import {
-  OwnershipTransferred,
-  RegistrationCreated,
-  ResourceRegistered
-} from "../generated/Platform/Platform"
-
-export function createOwnershipTransferredEvent(
-  previousOwner: Address,
-  newOwner: Address
-): OwnershipTransferred {
-  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
-    newMockEvent()
-  )
-
-  ownershipTransferredEvent.parameters = new Array()
-
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam(
-      "previousOwner",
-      ethereum.Value.fromAddress(previousOwner)
-    )
-  )
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
-  )
-
-  return ownershipTransferredEvent
-}
-
-export function createRegistrationCreatedEvent(
-  account: Address,
-  resourceType: i32,
-  requestId: BigInt
-): RegistrationCreated {
-  let registrationCreatedEvent = changetype<RegistrationCreated>(newMockEvent())
-
-  registrationCreatedEvent.parameters = new Array()
-
-  registrationCreatedEvent.parameters.push(
-    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
-  )
-  registrationCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "resourceType",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(resourceType))
-    )
-  )
-  registrationCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "requestId",
-      ethereum.Value.fromUnsignedBigInt(requestId)
-    )
-  )
-
-  return registrationCreatedEvent
-}
+import { newMockEvent } from "matchstick-as";
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts";
+import { ResourceRegistered } from "../generated/Platform/Platform";
 
 export function createResourceRegisteredEvent(
   account: Address,
   resourceType: i32,
-  id: BigInt,
+  id: i32,
   data: string
 ): ResourceRegistered {
-  let resourceRegisteredEvent = changetype<ResourceRegistered>(newMockEvent())
+  const resourceRegisteredEvent = changetype<ResourceRegistered>(newMockEvent());
 
-  resourceRegisteredEvent.parameters = new Array()
+  resourceRegisteredEvent.parameters = new Array();
 
   resourceRegisteredEvent.parameters.push(
     new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
-  )
+  );
+
   resourceRegisteredEvent.parameters.push(
     new ethereum.EventParam(
       "resourceType",
       ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(resourceType))
     )
-  )
+  );
+
   resourceRegisteredEvent.parameters.push(
-    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
-  )
+    new ethereum.EventParam(
+      "id",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(id))
+    )
+  );
+
   resourceRegisteredEvent.parameters.push(
     new ethereum.EventParam("data", ethereum.Value.fromString(data))
-  )
+  );
 
-  return resourceRegisteredEvent
+  return resourceRegisteredEvent;
 }
--- justicantus-subgraph/tests/platform.test.ts
+++ justicantus-subgraph/tests/platform.test.ts
@@ -3,58 +3,87 @@ import {
   describe,
   test,
   clearStore,
-  beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { OwnershipTransferred } from "../generated/schema"
-import { OwnershipTransferred as OwnershipTransferredEvent } from "../generated/Platform/Platform"
-import { handleOwnershipTransferred } from "../src/platform"
-import { createOwnershipTransferredEvent } from "./platform-utils"
+  afterEach
+} from "matchstick-as/assembly/index";
+
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Artist } from "../generated/schema";
+
+import { ResourceRegistered } from "../generated/Platform/Platform";
+
+import { handleResourceRegistered } from "../src/platform";
+import { createResourceRegisteredEvent } from "./platform-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
-describe("Describe entity assertions", () => {
-  beforeAll(() => {
-    let previousOwner = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let newOwner = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let newOwnershipTransferredEvent = createOwnershipTransferredEvent(
-      previousOwner,
-      newOwner
-    )
-    handleOwnershipTransferred(newOwnershipTransferredEvent)
-  })
+function createEventAndHandleResourceRegistration(resourceType: i32): ResourceRegistered {
+  const artistAddress = Address.fromString(
+    "0x0000000000000000000000000000000000000001"
+  );
+  const id = 0x1234;
+  const data = "First Artist";
 
-  afterAll(() => {
-    clearStore()
-  })
+  let resourceRegisteredEvent = createResourceRegisteredEvent(
+    artistAddress,
+    resourceType,
+    id,
+    data
+  );
+
+  handleResourceRegistered(resourceRegisteredEvent);
+
+  return resourceRegisteredEvent;
+};
+
+describe("Artist creation", () => {
+  afterEach(() => {
+    clearStore();
+  });
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("OwnershipTransferred created and stored", () => {
-    assert.entityCount("OwnershipTransferred", 1)
+  test("Artist created and stored", () => {
+
+    // NOTE: song resource type -> 2
+    let resourceRegisteredEvent = createEventAndHandleResourceRegistration(0x1);
+
+    assert.entityCount("Artist", 1);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
-    assert.fieldEquals(
-      "OwnershipTransferred",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "previousOwner",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "OwnershipTransferred",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "newOwner",
-      "0x0000000000000000000000000000000000000001"
-    )
+    const transaction = resourceRegisteredEvent.transaction;
+    const logIndex = resourceRegisteredEvent.logIndex;
 
-    // More assert options:
-    // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+    assert.fieldEquals(
+      "Artist",
+      transaction.hash.concatI32(logIndex.toI32()).toHexString(),
+      "account",
+      "0x0000000000000000000000000000000000000001"
+    );
+
+    assert.fieldEquals(
+      "Artist",
+      transaction.hash.concatI32(logIndex.toI32()).toHexString(),
+      "title",
+      "First Artist"
+    );
+
+    assert.fieldEquals(
+      "Artist",
+      transaction.hash.concatI32(logIndex.toI32()).toHexString(),
+      "platformId",
+      // 0x1234 to dec -> 4660
+      "4660"
+    );
+  });
+
+  describe("When resource registered is a song", () => {
+    test("Artist is not created", () => {
+      // NOTE: song resource type -> 2
+      let resourceRegisteredEvent = createEventAndHandleResourceRegistration(0x2);
+
+      assert.entityCount("Artist", 0);
+    });
+  });
+});
